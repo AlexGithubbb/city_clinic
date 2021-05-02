70b4d5be60e0290be816f5b0460da64e842028a9
alex <yaobowei333@gmail.com>
2021-05-02 10:42:29 -0400

    JIRA-3 import Swagger (not being used currently, use Postman instead) & common Result class & query with page info

--- service/service_hosp/src/main/java/com/alex/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/alex/hosp/controller/HospitalSetController.java
@@ -1,13 +1,15 @@
 package com.alex.hosp.controller;
 
 
+import com.alex.common.result.Result;
 import com.alex.hosp.service.HospitalSetService;
 import com.alex.model.hosp.HospitalSet;
+import com.alex.vo.hosp.HospitalSetQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -17,23 +19,67 @@ public class HospitalSetController {
     @Autowired
     HospitalSetService hospitalSetService;
 
-    @RequestMapping("/set")
-    public void addHosp(@RequestParam("hosname") String hosname, @RequestParam("hoscode") String hoscode){
-        HospitalSet hospitalSet = new HospitalSet();
-        hospitalSet.setHosname(hosname);
-        hospitalSet.setHoscode(hoscode);
-        boolean save = hospitalSetService.save(hospitalSet);
 
-        System.out.println("inserted? " + save);
-    }
 
-    @RequestMapping("/getall")
-    public List<HospitalSet> getAllHospSet(){
-
+    @RequestMapping("/getAll")
+    public  Result getAllHospSet(){
         List<HospitalSet> list = hospitalSetService.list();
-        return list;
-
+        return Result.ok(list);
     }
 
+    @DeleteMapping("/{id}")
+    public Result testDeleteById(@PathVariable Long id){
+        boolean flag = hospitalSetService.removeById(id);
+        return Result.flag(flag);
+    }
+
+    /**
+     *
+     * 条件查询 + 接口
+     * @ReqestBody is bonded with @PostMapping
+     * @param current
+     * @param limit
+     * @param hospitalSetQueryVo
+     * @return
+     */
+    @PostMapping("/page/{current}/{limit}")
+    public Result queryByPage(
+                             @PathVariable("current") long current,
+                             @PathVariable("limit") long limit,
+                            @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo){
+        Page<HospitalSet> hospitalSetPage = new Page<>(current, limit);
+
+        // config wrapper
+
+        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
+
+        String hosname="";
+        String hoscode="";
+        if(hospitalSetQueryVo != null){
+
+           hosname = hospitalSetQueryVo.getHosname();
+           hoscode = hospitalSetQueryVo.getHoscode();
+        }
+
+        // decide if the input value is valid or not, this logic should be put in service level
+        if(!StringUtils.isEmpty(hosname) ){
+            wrapper.like("hosname",hosname);
+        }
+
+        if(!StringUtils.isEmpty(hoscode)){
+            wrapper.eq("hoscode",hoscode);
+        }
+
+        Page<HospitalSet> page = hospitalSetService.page(hospitalSetPage, wrapper);
+//
+//        long total = page.getTotal();
+//        long currentPage = page.getCurrent();
+//        List<HospitalSet> records = page.getRecords();
+//        boolean b = page.hasNext();
+//        boolean hasPrevious = page.hasPrevious();
+
+        return Result.ok(page);
+
+    }
 
 }
